138e7fa51d820ed416f33b63e7f2f78782779480
JohnJosephLanda <jjl2008.com@gmail.com>
2023-02-07 08:21:03 -0500

    Changed fishcode to GameCode

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import fishcode as game
+import GameCode as game
 import FishEatFishHomepage as homepage
 
 # Press the green button in the gutter to run the script.
