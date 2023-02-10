279bb9d1e3a9f8f6adad322d8467289ddfd50ee8
brookefol <bfoley2026@gmail.com>
2023-02-10 11:23:08 -0500

    Created to close start screenbut closes whole game

--- FishEatFishHomepage.py
+++ FishEatFishHomepage.py
@@ -125,9 +125,13 @@ class Homepage(arcade.Window):
         pass
 
     def on_start(self, event):
+        # may not work
         game_view = gc.MyGame()
         game_view.setup()
         arcade.window_commands.set_window(game_view)
+        # Closes the entire game, meant to close the start screen only
+        Homepage.close(self)
+        
 
         
 def main():
