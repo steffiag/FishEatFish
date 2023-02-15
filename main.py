b923f4c561b974fbde2885698f6884d9332c6cf3
JohnJosephLanda <jjl2008.com@gmail.com>
2023-02-15 11:07:49 -0500

    simplified main

--- main.py
+++ main.py
@@ -3,9 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import GameCode as game
 import FishEatFishHomepage as homepage
-import EndScreen as ending
 
 play = True
 
