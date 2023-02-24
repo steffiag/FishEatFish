6400ab6134ef1b30e758eb688f973fe6d1a21350
JohnJosephLanda <jjl2008.com@gmail.com>
2023-02-24 11:15:45 -0500

    edited info button

--- FishEatFishHomepage.py
+++ FishEatFishHomepage.py
@@ -91,7 +91,7 @@ class Homepage(arcade.Window):
             width=300,
             height=200,
             message_text=(
-                "You are the pink fish. You need to eat fish that are smaller than you. Eating a smaller fish increases your size. If you eat a fish thats bigger than you, you die and game over!"
+                "You are the pink fish. You need to eat fish that are smaller than you. Eating a smaller fish increases your size. If you eat a fish thats bigger than you, you die and game over! (also, you may insta-die due to the fish being able to spawn on top of you)"
             ),
             callback=self.on_message_box_close,
             buttons=["Ok"]
